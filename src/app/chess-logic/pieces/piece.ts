ae094d3be311a610ed621255f83370fcc02ec52b
Saahil <saahilrarora@gmail.com>
2025-01-22 14:27:52 -0800

    Initial commit for chess game project

---
+++ src/app/chess-logic/pieces/piece.ts
@@ -0,0 +1,19 @@
+export abstract class Piece{
+    protected abstract _FENChar: FENChar;
+    protected abstract _directions: Coords[];
+
+    constructor(private _color: Color){}
+
+    public get FENChar(): FENChar{
+        return this._FENChar;
+
+    }
+
+    public get directions(): Coords[]{
+        return this._directions;
+    }
+
+    public get color(): Color{
+        return this._color;
+    }
+}
---
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chess-board',
+  imports: [],
+  templateUrl: './chess-board.component.html',
+  styleUrl: './chess-board.component.css'
+})
+export class ChessBoardComponent {
+
+}
